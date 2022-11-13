3e37c2285ad90e3752b71a3b126ac3e39ef8903a
Jack Agtual <jaagtual@gmail.com>
2022-11-13 13:46:14 -0800

    Adding function to add task

--- src/index.js
+++ src/index.js
@@ -7,4 +7,6 @@ const domController = TaskDom();
 const taskMgr = TaskMgr();
 
 domController.clearTasks();
+taskMgr.addTask('Title 1', 'a description for task 1', 'Project 1', '11/14/22')
+taskMgr.addTask('Title 2', 'a description for task 2', 'Project 1', '11/14/22')
 domController.addTaskList(taskMgr.getTaskList());
--- src/taskMgr.js
+++ src/taskMgr.js
@@ -1,34 +1,14 @@
 export default function TaskMgr() {
-    const _tasks = [
-        {
-            title: 'Title 1',
-            description: 'This is the description for task 1',
-            project: 'This task belongs to project 1',
-            dueDate: '11/15/22'
-        },
-        {
-            title: 'Title 2',
-            description: 'This is the description for task 2',
-            project: 'This task belongs to project 2',
-            dueDate: '11/15/22'
-        },
-        {
-            title: 'Title 3',
-            description: 'This is the description for task 3',
-            project: 'This task belongs to project 3',
-            dueDate: '11/15/22'
-        },
-        {
-            title: 'Title 4',
-            description: 'This is the description for task 4',
-            project: 'This task belongs to project 4',
-            dueDate: '11/15/22'
-        },
-    ];
+    const _tasks = [];
 
     const getTaskList = () => _tasks;
 
+    const addTask = (title, description, project, dueDate) => {
+        _tasks.push({title, description, project, dueDate});
+    }
+
     return {
-        getTaskList
+        getTaskList,
+        addTask
     }
 }
