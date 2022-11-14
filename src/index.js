5dd7ae4debe5197f573db49bc3363334863c0333
Jack Agtual <jaagtual@gmail.com>
2022-11-13 22:16:28 -0800

    Adding form to add new task; hard-coding project drop down

--- src/index.js
+++ src/index.js
@@ -6,7 +6,5 @@ import TaskMgr from './taskMgr'
 const domController = TaskDom();
 const taskMgr = TaskMgr();
 
-domController.clearTasks();
-taskMgr.addTask('Title 1', 'a description for task 1', 'Project 1', '11/14/22')
-taskMgr.addTask('Title 2', 'a description for task 2', 'Project 1', '11/14/22')
-domController.addTaskList(taskMgr.getTaskList());
+const addTask = document.querySelector('#add-task');
+addTask.addEventListener('click', domController.inputNewTask)
--- src/taskDom.js
+++ src/taskDom.js
@@ -2,13 +2,20 @@ import pencil  from './icons/pencil.svg'
 import trash from './icons/trash.svg'
 
 export default function TaskDom() {
+
+    const _getContent = () => document.getElementById('content');
+    let _currentlyInputtingTask = false;
+    const _tasks = [];
+    
     const clearTasks = () => {
-        const content = document.querySelector('#content');
+        const content = _getContent();
         content.innerHTML = '';
     }
 
     const addTask = task => {
-        const content = document.querySelector('#content');
+        _tasks.push(task);
+        
+        const content = _getContent()
 
         const taskDiv = document.createElement('div');
         taskDiv.classList.add('task');
@@ -32,9 +39,68 @@ export default function TaskDom() {
         });
     }
 
+    const inputNewTask = () => {
+        if (_currentlyInputtingTask) return;
+        _currentlyInputtingTask = true;
+
+        const content = _getContent();
+        
+        const form = document.createElement('form');
+
+        form.innerHTML = `
+            <div class="form-element">
+                <label for="title">Title:</label>
+                <input type="text" id="title">
+            </div>
+            
+            <div class="form-element">
+                <label for="description">Description</label>
+                <input type="text" id="description">
+            </div>
+
+            <div class="form-element">
+                <label for="project">Project:</label>
+                <select name="project" id="project">
+                    <option value="none"></option>
+                    <option value="Project1">Project1</option>
+                    <option value="Project2">Project2</option>
+                    <option value="Project3">Project3</option>
+                </select>
+            </div>
+
+            <div class="form-element">
+                <label for="due-date">Due date:</label>
+                <input type="date" id="due-date">
+            </div>
+
+            <button id="add-task-submit">Add</button>
+        `;
+
+        content.appendChild(form);
+
+        const submitBtn = document.getElementById('add-task-submit');
+        submitBtn.addEventListener('click', e => {
+            e.preventDefault();
+
+            _currentlyInputtingTask = false;
+
+            addTask({
+                title: document.getElementById('title').value,
+                description: document.getElementById('description').value,
+                project: document.getElementById('project').value,
+                dueDate: document.getElementById('due-date').value,
+            });
+
+            form.remove();
+
+            console.log(_tasks)
+        })
+    }
+
     return {
         clearTasks,
         addTask,
-        addTaskList
+        addTaskList,
+        inputNewTask
     };
 }
